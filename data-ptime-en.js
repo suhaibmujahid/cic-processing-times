79809b279f4b1edf90ace7f7618b3e8e827e4676
Suhaib Mujahid <suhaibmujahid@gmail.com>
2021-11-03 15:31:30 -0400

    Format the data

---
+++ data-ptime-en.js
@@ -0,0 +1,29 @@
+import { readJSON, writeCSV } from "https://deno.land/x/flat@0.0.13/mod.ts";
+
+const filename = Deno.args[0];
+const data = await readJSON(filename);
+const output = [];
+for (const type in data) {
+  output.push({ type, ...data[type] });
+}
+const newfile = filename.replace(".json", ".csv");
+await writeCSV(newfile, output);
+
+const formate = (keysMap, obj) =>
+  Object.keys(obj).reduce(
+    (acc, key) => ({
+      ...acc,
+      ...{
+        [keysMap[key] || key]: obj[key].replace(
+          "No processing time available",
+          ""
+        ),
+      },
+    }),
+    {}
+  );
+
+const country = (await readJSON("data-country-name-en.json"))["country-name"];
+const formattedOutput = output.map((row) => formate(country, row));
+const formattedfile = `formatted-${newfile}`;
+await writeCSV(formattedfile, formattedOutput);
